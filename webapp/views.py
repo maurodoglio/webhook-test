a009e3cf0c4186aeddb5a00dc2dca05f433dd26a
Mauro Doglio <mdoglio@mozilla.com>
2015-06-02 16:21:40 +0100

    Add rest_framework to requirements

--- webapp/views.py
+++ webapp/views.py
@@ -11,11 +11,21 @@ class GithubEventSerializer(serializers.ModelSerializer):
     def to_internal_value(self, data):
         return {
             'created_on': datetime.now(),
-            'payload': json.dumps(data, indent=4)
+            'payload': json.dumps(data)
         }
 
     def create(self, validated_data):
         return GithubEvent.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.payload = validated_data
+        return instance
+
+    def to_representation(self, instance):
+        if not instance:
+            return None
+        instance.payload = json.loads(instance.payload)
+        return super(GithubEventSerializer, self).to_representation(instance)
 
 
 class GithubEventViewSet(viewsets.ModelViewSet):
