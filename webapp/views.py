74830d39ae6521534e34e34727a86a9822867a6a
Mauro Doglio <mdoglio@mozilla.com>
2015-06-03 11:21:12 +0100

    First pr

--- webapp/views.py
+++ webapp/views.py
@@ -4,28 +4,29 @@ from rest_framework import viewsets, serializers
 from webapp.models import GithubEvent
 
 
-class GithubEventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GithubEvent
+class GithubEventSerializer(serializers.BaseSerializer):
 
     def to_internal_value(self, data):
         return {
-            'created_on': datetime.now(),
             'payload': json.dumps(data)
         }
+
+    def to_representation(self, instance):
+        instance.payload = json.loads(instance.payload)
+        return super(GithubEventSerializer, self).to_representation(instance)
 
     def create(self, validated_data):
         return GithubEvent.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.payload = validated_data
+        instance.payload = validated_data.payload
         return instance
 
-    def to_representation(self, instance):
-        if not instance:
-            return None
-        instance.payload = json.loads(instance.payload)
-        return super(GithubEventSerializer, self).to_representation(instance)
+    # def to_representation(self, instance):
+    #     if not instance:
+    #         return None
+    #     instance.payload = json.loads(instance.payload)
+    #     return super(GithubEventSerializer, self).to_representation(instance)
 
 
 class GithubEventViewSet(viewsets.ModelViewSet):
